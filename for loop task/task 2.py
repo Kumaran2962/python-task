4219bc81b6cfc5a074f5668518a1946e2a59393f
“kumaran2962” <kumaran2962@gmail.com>
2023-06-10 22:36:53 +0530

    task done

---
+++ for loop task/task 2.py
@@ -0,0 +1,6 @@
+i=int(input("enter the number: "))
+a=int(input("enter the number: "))
+char=input("enter the char which is to be used for matrix: ")
+for i in range(1,i+1):
+    print(char *a)
+print(f"matrix-{i}*{i}")

1e2d15919c64fcc840fe051a51bd6b5c69ccfa87
“kumaran2962” <kumaran2962@gmail.com>
2023-06-16 18:45:22 +0530

    finished program

---
+++ general program/matrix numbers.py
@@ -0,0 +1,5 @@
+a=25
+for i in range(1,a+1):
+    print(i,end="\t")
+    if(i%5==0):
+        print()
---
+++ general program/printing pattern.py
@@ -0,0 +1,4 @@
+i=int(input("enter the number: "))
+char=input("enter the char which is to be used: ")
+for i in range(i,0,-1):
+    print(char * i)

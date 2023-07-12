945923cd9169ddefe88fbf03b0f4fefdd21992c3
“kumaran2962” <kumaran2962@gmail.com>
2023-07-12 19:33:46 +0530

    work done

---
+++ class task/function.py
@@ -0,0 +1,31 @@
+def addition():
+    a=int(input("enter the first number: "))
+    b=int(input("enter the second number: "))
+    print(f"{a} + {b} ={a+b}")
+def subraction():
+    a=int(input("enter the first number: "))
+    b=int(input("enter the second number: "))
+    print(f"{a} - {b} ={a-b}")
+def multiplication():
+    a=int(input("enter the first number: "))
+    b=int(input("enter the second number: "))
+    print(f"{a} * {b} ={a*b}")
+def division():
+    a=int(input("enter the first number: "))
+    b=int(input("enter the second number: "))
+    print(f"{a} / {b} ={a/b}")
+def float_division():
+    a=int(input("enter the first number: "))
+    b=int(input("enter the second number: "))
+    print(f"{a} // {b} ={a//b}")
+def swap():
+    a=int(input("enter the first number: "))
+    b=int(input("enter the second number: "))
+    print(f"{a} + {b} ={a+b}")
+
+
+addition()
+subraction()
+multiplication()
+division()
+float_division()
